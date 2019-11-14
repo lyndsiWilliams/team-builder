fa13f3ee67ec9310298ec5d129dd2d43cd301dca
lyndsiWilliams <kcatgirl@gmail.com>
2019-11-13 18:50:28 -0600

    Members now display on a reactstrap card.

--- team-builder/src/components/Members.js
+++ team-builder/src/components/Members.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { Card, Button, CardTitle, CardText, CardDeck, CardSubtitle, CardBody } from 'reactstrap';
 
 const Members = props => {
   return (
-    <div className="member-list">
-      {props.members.map(member => (
-        <div className="member" key={member.id}>
-          <h2>{member.name}</h2>
-          <p>{member.email}</p>
-          <p>{member.role}</p>
-        </div>
-      ))}
-    </div>
+    <CardDeck>
+        {props.members.map(member => (
+        <Card key={member.id}>
+            {/* <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" /> */}
+            <CardBody>
+            <CardTitle>{member.name}</CardTitle>
+            <CardSubtitle>{member.email}</CardSubtitle>
+            <CardText>{member.role}</CardText>
+            <Button>Edit</Button>
+            </CardBody>
+        </Card>
+        ))}
+    </CardDeck>
   );
 };
 
